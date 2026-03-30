67a07dc923045584e002b4b99940ff14392eff24
Abu Sayed Muhammed Hanif <167633635+asm-hanif@users.noreply.github.com>
2026-03-30 17:18:23 +0600

    Add DataStoreManager for preference data storage

---
+++ DataStoreManager.kt
@@ -0,0 +1,6 @@
+package com.hanif.nagarfix
+
+import android.content.Context
+import androidx.datastore.preferences.preferencesDataStore
+
+val Context.dataStore by preferencesDataStore(name = "settings")
